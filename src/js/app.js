aa70977b03e99b8d0be0cce7b447a10ca267ee8c
Tom Schrimshaw <tschrimshaw@gmail.com>
2020-11-09 15:52:16 +0000

    feat: radios to choose file origin
    now have two radio btns to choose where to look for
    the bookmarks file,
    from appdata (stream) or downloaded file.

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,28 @@
 import ListBuilder from './listBuilder'
 
 const init = () => {
-    Client.readStream()
+    //TODO: create radio / select / file chooser to choose either exported html file or local appData file
+    const fileSelector = document.querySelector('.file-selector')
+    fileSelector.addEventListener('change', readOrigin)
+
+    //fire off initial event to populate page with default file selector form option
+    const initEvent = new Event('change')
+    fileSelector.dispatchEvent(initEvent)
+}
+
+const readOrigin = (evt) => {
+    evt.preventDefault()
+    const origin = evt.target.value
+
+    //clear current lists
+    document.querySelector('#lists-container').innerHTML = ''
+
+    //dependent on selected origin return helper method to run
+    const helperToRun = origin === 'file'
+        ? Client.readFile
+        : Client.readStream
+
+    helperToRun()
     .then((data) => {
         displayBookMarks(data)
     })
